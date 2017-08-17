49e09421218d26034e3900feb8694c8a1c1b0da1
Emmanuel King Kasulani <kasulani@gmail.com>
2017-08-17 12:11:54 +0300

    added a config file for the shopping list application and log file for the application

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,28 @@
     Email: kasulani@gmail.com
 """
 
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+# load the config file in instance folder, don't suppress errors (silent=false)
+app.config.from_pyfile('config.cfg', silent=False)
+logfile = app.config['LOGFILE']
+log_level = app.config['LEVEL']
+# set logging format
+formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+# set up file handler for the logger
+handler = RotatingFileHandler(logfile, maxBytes=10000000, backupCount=5)
+# set logging level as set in the config file
+if log_level == 'DEBUG':
+    handler.setLevel(logging.DEBUG)
+else:
+    handler.setLevel(logging.INFO)
+
+handler.setFormatter(formatter)
+app.logger.setLevel(log_level)
+app.logger.addHandler(handler)
 
 from app import views
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 from app import app
 
-app.run(debug=True)
+app.logger.debug("Starting Shopping List application...\n")
+app.run(host='127.0.0.1',
+        port=app.config['PORT'],
+        use_reloader=False,
+        debug=app.config['DEBUG'])
